2214e412e2c823e0b85bba1448150b42219778ee
GB380 <47633777+GB380@users.noreply.github.com>
2020-09-10 11:48:21 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+class Main
+{
+public static void main( String args[] )
+   {
+       String sn=BIO.getString();//get the input from BIO class
+       
+       while (sn!=("END")){//sn stands for student number
+           
+       int mm=BIO.getInt();
+       if (mm < 40) {
+           result="Fail";
+           else result="Pass";
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+    }
+}
+}  
+} 
+    
+  
+        
+   
+    
+    
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2.1 Java Exercise (Pass Fail)
+ Exercise 2.1 of Learning Java
